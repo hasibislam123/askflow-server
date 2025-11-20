26158029778e22a99254f91a07ac415af864cbde
Hasib Islam <hasib64dj@gmail.com>
2025-11-20 15:22:02 +0600

    added the new code

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const cors = require('cors')
 const app = express()
 require('dotenv').config();
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+
 const port = process.env.PORT || 3000
 
 
@@ -11,10 +15,64 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 app.use(cors())
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@ggbd.znymale.mongodb.net/?appName=ggbd`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+   serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+   }
+});
+
+
+async function run() {
+   try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+
+      const db = client.db('askflow_db');
+      const parcelsColletion = db.collection('parcels');
+
+      // parcelApi
+      // Get
+      app.get('/parcels', async (req, res) => {
+         const query = {}
+         const {email} = req.query;
+
+         if(email){
+            query.senderEmail = email;
+         }
+
+         const cursor = parcelsColletion.find(query);
+         const result = await cursor.toArray();
+         res.send(result);
+      })
+
+      // Post
+      app.post('/parcels', async (req, res) => {
+         const parcel = req.body;
+         const result = await parcelsColletion.insertOne(parcel)
+         res.send(result)
+      })
+
+
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+   } finally {
+      // Ensures that the client will close when you finish/error
+      //  await client.close();
+   }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
-  res.send(' askFlow in Hello World!')
+   res.send(' askFlow in Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+   console.log(`Example app listening on port ${port}`)
 })
