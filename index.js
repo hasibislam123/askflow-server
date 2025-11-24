33c49c56c8d1e4f07720e8ba0d642446f3278d7e
Hasib Islam <hasib64dj@gmail.com>
2025-11-24 16:48:33 +0600

    added the new code

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET);
 const port = process.env.PORT || 3000
 const crypto = require("crypto");
 
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./askflow-firebase-adminsdk.json");
+
+admin.initializeApp({
+   credential: admin.credential.cert(serviceAccount)
+});
+
 function generateTrackingId() {
    const prefix = "PRCL";
    const date = new Date().toISOString().slice(0, 10).replace(/-/g, "");
@@ -19,6 +27,26 @@ function generateTrackingId() {
 // middleware
 app.use(express.json());
 app.use(cors());
+
+const variFyFBToken = async (req, res, next) => {
+   const token = req.headers.authorization
+   if (!token) {
+      return res.status(401).send({ message: 'unauthorized access' })
+   }
+   try {
+      const idToken = token.split(' ')[1];
+      const decoded = await admin.auth().verifyIdToken(idToken)
+      console.log('decoded in the token', decoded);
+      req.decoded_email = decoded.email;
+      next()
+   }
+   catch (err) {
+      return res.status(401).send({ message: 'unauthorized access' });
+   }
+
+
+
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@ggbd.znymale.mongodb.net/?appName=ggbd`;
@@ -39,8 +67,27 @@ async function run() {
       await client.connect();
 
       const db = client.db('askflow_db');
+      const userCollection = db.collection('users');
       const parcelsCollection = db.collection('parcels');
       const paymentCollection = db.collection('payments');
+      const ridersCollection = db.collection('riders');
+
+      // user related  apis
+      app.post('/users', async (req, res) => {
+         const user = req.body;
+         user.role = 'user';
+         user.createdAt = new Date();
+
+         const email = user.email
+         const userExists = await userCollection.findOne({ email })
+
+         if (userExists) {
+            return res.send({ message: 'user exists' })
+         }
+
+         const result = await userCollection.insertOne(user);
+         res.send(result);
+      })
 
       // parcel api
       app.get('/parcels', async (req, res) => {
@@ -202,16 +249,75 @@ async function run() {
 
       // payment related api 
 
-      app.get('/payments', async (req, res) =>{
+      app.get('/payments', variFyFBToken, async (req, res) => {
          const email = req.query.email;
-         const query= {}
-         if(email) {
-            query.customerEmail = email
+         const query = {}
+
+         // console.log( 'headers', req.headers);
+
+         if (email) {
+            query.customerEmail = email;
+
+            // check email address
+            if (email !== req.decoded_email) {
+               return res.status(403).send({ message: 'forbidden access' })
+            }
          }
-         const cursor = paymentCollection.find(query);
+         const cursor = paymentCollection.find(query).sort({ paidAt: -1 });
          const result = await cursor.toArray();
-         res.send(result)
+         res.send(result);
       })
+
+
+      //  riders retated apis 
+
+      app.get('/riders', async (req, res) => {
+         const query = {}
+         if (req.query.status) {
+            query.status = res.query.status;
+         }
+         const cursor = ridersCollection.find(query)
+         const result = await cursor.toArray();
+         res.send(result);
+
+      })
+
+      app.post('/riders', async (req, res) => {
+         const rider = req.body;
+         rider.status = 'pending';
+         rider.createdAt = new Date();
+
+         const result = await ridersCollection.insertOne(rider)
+         res.send(result);
+
+
+      })
+
+      app.patch('/riders/:id', variFyFBToken, async (req, res) => {
+         const status = req.body.status;
+         const id = req.params.id;
+         const query = { _id: new ObjectId(id) }
+         const updatedDoc = {
+            $set: {
+               status: status
+            }
+         }
+         const result = await ridersCollection.updateOne(query, updatedDoc)
+
+         if (status === 'approved') {
+            const email = req.body.email;
+            const userQuery = { email }
+            const updateUser = {
+               $set: {
+                  role: 'rider'
+               }
+            }
+            const userResult = await userCollection.updateOne(userQuery, updateUser);
+         }
+
+         res.send(result);
+      })
+
 
       // Send a ping to confirm a successful connection
       await client.db("admin").command({ ping: 1 });
